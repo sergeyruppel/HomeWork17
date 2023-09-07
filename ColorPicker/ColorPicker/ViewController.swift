e1a681ce8f897ac0f78a680f81edd0da6a0d3237
Sergey Ruppel <sergeyruppel@gmail.com>
2023-09-07 11:47:12 +0700

    Project created

---
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Sergey Ruppel on 07.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
